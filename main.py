29c7adbe77c8613956e94d983c601c43bd364d6d
Soebb <87156166+Soebb@users.noreply.github.com>
2021-08-07 11:20:18 +0430

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import asyncio
 import os, unittest, time, datetime
 import urllib.request, urllib.error, urllib.parse
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -40,7 +39,8 @@ async def link_handler(bot, message):
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options)
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
     driver.get(url)
     time.sleep(5)
     dt=datetime.datetime.now().strftime("%Y%m%d%H%M")
